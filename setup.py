e0401acd504e5471d9031590a23e62b0b6596822
Florian Schulze <florian.schulze@gmx.net>
2007-09-04 22:08:20 +0000

    Updating version on trunk after tagging
    svn path=/buildout/plone.recipe.zope2install/trunk/; revision=48708

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import os
 from setuptools import setup, find_packages
 
 name = "plone.recipe.zope2install"
-version = '0.2'
+version = '0.3'
 
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
