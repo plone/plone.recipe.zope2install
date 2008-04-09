a46928fd2477d59c7554ee7ec3d7fbe875179961
Tim Knapp <tim@emergetec.com>
2008-04-09 20:02:03 +0000

    Changing zope-version option to zope2-instance recipe.
    svn path=/buildout/plone.recipe.zope2install/trunk/; revision=62362

--- src/plone/recipe/zope2install/__init__.py
+++ src/plone/recipe/zope2install/__init__.py
@@ -47,18 +47,6 @@ class Recipe:
         buildout['buildout'].setdefault(
                     'download-cache',
                     os.path.join(buildout['buildout']['directory'], 'downloads'))
-        
-        # Retrieve Zope version
-        # Zope 2.7/2.8
-        if os.path.isfile(os.path.join(options['location'], 'lib/python') + '/version.txt'):
-            version_file = open(os.path.join(options['location'], 'lib/python') + '/version.txt')
-            options['zope2-version'] = version_file.read()
-        # Zope 2.9+
-        elif os.path.isfile(os.path.join(options['location'], 'lib/python/Zope2') + '/version.txt'):
-            version_file = os.path.join(options['location'], 'lib/python/Zope2') + '/version.txt'
-            options['zope2-version'] = version_file.read()
-        else:
-            options['zope2-version'] = "(UNKNOWN)"
 
     def install(self):
         options = self.options
