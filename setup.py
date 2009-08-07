ce5f38d1e1fa4467c29b07f226a2152b37f7f9fb
Godefroid Chapelle <gotcha@bubblenet.be>
2009-08-07 20:24:26 +0000

    Preparing release 3.2
    svn path=/buildout/plone.recipe.zope2install/trunk/; revision=94037

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import os
 from setuptools import setup, find_packages
 
 name = "plone.recipe.zope2install"
-version = '3.2dev'
+version = '3.2'
 
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
